35c80e8e5193ae17b15df4837daaadf2875d0fbd
Mashrab Yo'ldoshev <mashrabyoldoshev@Mashrabs-MacBook-Air.local>
2023-12-29 05:51:19 +0500

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./custom-button";
+import AIPicker from "./ai-picker";
+import ColorPicker from "./color-picker";
+import FilePicker from "./file-picker";
+import Tab from "./tab";
+
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
